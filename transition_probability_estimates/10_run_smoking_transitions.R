b3f9c0c14985e378134e90917189984cb2471fe7
Duncan Gillespie <duncan.gillespie@sheffield.ac.uk>
2026-02-14 22:12:39 +0000

    run England ests with 1k uncertainty samples

--- transition_probability_estimates/10_run_smoking_transitions.R
+++ transition_probability_estimates/10_run_smoking_transitions.R
@@ -70,7 +70,7 @@ config_eng <- list(
   smooth_rate_dim_relapse = c(15, 7), k_smooth_age_relapse = 6, age_trend_limit_relapse = 79,
   
   # Uncertainty Params
-  kn = 50, kR = 0.9, kn_samp = 3,
+  kn = 50, kR = 0.9, kn_samp = 1000,
   cont_limit = 2019
 )
 
