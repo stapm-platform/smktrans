c4836685e0c329bba559748d1d4a474c4e74e9c0
Duncan Gillespie <duncan.gillespie@sheffield.ac.uk>
2026-03-26 14:10:33 +0000

    add renv

--- 03_load_packages.R
+++ 03_load_packages.R
@@ -1,30 +1,36 @@
+# ==============================================================================
+# Script: load_packages.R
+# Purpose: Load all required dependencies for the smktrans project
+# ==============================================================================
 
-# The aim of this script is to load the packages required
-# to run the simulation and process the results
-
-# CRAN packages
+# 1. Core Data Manipulation & Utilities
 library(data.table)
+library(matrixStats)
 library(stringr)
-library(flextable)
 library(magrittr)
-library(plyr)
+
+# 2. Reading & Writing Data
 library(readxl)
 library(openxlsx)
+
+# 3. Stats & Testing
+library(survey)
 library(testthat)
+
+# 4. Visualization & Reporting
+library(ggplot2)
 library(cowplot)
 library(RColorBrewer)
-library(ggplot2)
 library(viridis)
-library(survey)
-library(matrixStats)
 library(plotly)
 library(DT)
 library(crosstalk)
 library(htmltools)
+#library(flextable)
 library(gt)
 library(gtsummary)
 
-# STAPM packages
+# 5. STAPM Packages (Loaded LAST to ensure their functions take priority)
 library(stapmr)
 library(tobalcepi)
 library(hseclean)
@@ -32,5 +38,15 @@ library(smktrans)
 library(mort.tools)
 library(toolkitr)
 
+# Print warnings as they occur
 options(warn = 1)
+
+# Maximize data.table performance by using all available CPU cores
+data.table::setDTthreads(0)
+cat("data.table is using", getDTthreads(), "threads.\n")
+
+
+
+
+
 
